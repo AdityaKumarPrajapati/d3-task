591f50129a4d2a28066a0911ff6931e21e2f5955
AdityKumarPrajapati <aditya.prajapati@successive.tech>
2020-08-31 09:36:50 +0530

    anguar-d3

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartComponent } from './chart/chart.component';
+import { LineComponent } from './line/line.component';
 
 
 
 @NgModule({
-  declarations: [ChartComponent],
+  declarations: [ChartComponent, LineComponent],
   imports: [
     CommonModule
   ],
